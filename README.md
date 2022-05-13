fbca1c1ab3194647df03b41a2956914e99113f37
lionelsu <lionel_suicune@hotmail.com>
2022-05-13 15:58:55 -0400

    entrega do projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+# Repositório e-Commerce
+
+###
+
+O desafio consiste em montar um e-Commerce consumindo uma API do zero.
+A REST API do comercio de livros foi construída em C# com base no repositório da mentora.
+
+O e-Commerce foi construído no Angular 13, foi um grande desafio pois, como iniciante no Angular e no Java eu precisei estudar bem a tipagem do mesmo.
+
+Agradeço novamente a oportunidade e espero poder aprender muito mais com a comunidade.
---
+++ desafioE-Commerce/src/app/components/bookstore-app/product-list/product-list.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Book } from "../product-list/product-item/model/Book";
+
+export const books: Book[] = [
+  { id: '1', name: 'Book1' , price: 10, quantity: 1, category: "Acao", img:"img1"},
+  { id: '2', name: 'Book2' , price: 10, quantity: 1, category: "Acao", img:"img2"},
+  { id: '3', name: 'Book3' , price: 10, quantity: 1, category: "Acao", img:"img3"},
+
+];
+
+@Injectable()
+
+export class BooksService {
+  private url = 'https://localhost:4438/api/bookstore';
+
+  httpOptions= {
+    Headers: new HttpHeaders ({ 'content-type': 'application/json' })
+  }
+
+  constructor(private http:HttpClient){}
+
+  getBook(){
+    return this.http.get(this.url)
+  }
+
+}
